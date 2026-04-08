5a8888bf4f86507d7cfc296f79e03c9a8e8ef35f
jdoria04 <166972533+jdoria04@users.noreply.github.com>
2026-04-07 23:24:09 -0500

    Corrección de caché

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'uninota-pro-v1';
+const CACHE_NAME = 'uninota-pro-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './styles.css',
-  './script.js',
+  './styles.css?v=2',
+  './script.js?v=2',
   './manifest.json',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=DM+Mono:wght@500;600&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
